f8a8bf2ecdee7eec7ab36e10a3796c5b08c7f381
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2022-11-17 23:45:01 -0300

    ex 4 finalizado

--- Exercícios - Subrotinas, Pilhas e Pilha de Execução/mips4.asm
+++ Exercícios - Subrotinas, Pilhas e Pilha de Execução/mips4.asm
@@ -7,7 +7,126 @@
 # desejada. Respeite as convenções de uso dos registradores.
 
 .data
-	Pergunta: .asciiz "Qual forma geométrica você deseja calcular a área(1-Circunferência, 2-Triângulo e 3-Retângulo)? "
-	reposta: .asciiz "Cálculo da área: "
+	pergunta: .asciiz "Qual forma geométrica você deseja calcular a área(1-Circunferência, 2-Triângulo e 3-Retângulo)? "
+	perguntaR: .asciiz "Informe o r: "
+	perguntaA: .asciiz "Informe o a: "
+	perguntaB: .asciiz "Agora informe o b: "
+	resposta: .asciiz "Cálculo da área: "
 .text
-	  
+	li $v0, 4		#imprime a string
+	la $a0, pergunta	#carrega o endereço da string
+	syscall
+	
+	li $v0, 5			#lê um inteiro
+	syscall
+	move $t0, $v0			#guarda a opção do usuário
+	
+	beq $t0, 1, circunferencia	#se digitar 1 pula pra circunferência
+	beq $t0, 2, triangulo		#se digitar 2 pula pra triângulo
+	beq $t0, 3, retangulo		#se digitar 3 pula pra retângulo
+
+	li $v0, 10	#se não digitar nada válido, encerra o programa
+	syscall
+
+circunferencia:
+	jal areaDaCirc 	#chamada da função
+	
+	li $v0, 4		# imprime string
+	la $a0, resposta	# carrega o endereço da string
+	syscall
+	
+	li $v0, 1		# imprime inteiro
+	move $a0, $v1		# carrega em a0 o resultado do cálculo
+	syscall
+	
+	li $v0, 10	# encerra o programa
+	syscall
+
+triangulo:
+	jal areaDoTri	#chamada da função
+	
+	li $v0, 4		# imprime string
+	la $a0, resposta	# carrega o endereço da string
+	syscall
+	
+	li $v0, 1		# imprime inteiro
+	move $a0, $v1		# carrega em a0 o resultado do cálculo
+	syscall
+	
+	li $v0, 10	# encerra o programa
+	syscall
+
+retangulo:
+	jal arearet	#chamada da função
+	
+	li $v0, 4		# imprime string
+	la $a0, resposta	# carrega o endereço da string
+	syscall
+	
+	li $v0, 1		# imprime inteiro
+	move $a0, $v1		# carrega em a0 o resultado do cálculo
+	syscall
+	
+	li $v0, 10	# encerra o programa
+	syscall
+
+areaDaCirc:
+	li $v0, 4
+	la $a0, perguntaR
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	mult $t0, $t0		# raio ao quadrado
+	mflo $t0
+	addi $t1, $zero, 3	# vai ser o PI
+	mult $t0, $t1		# 3 * r²
+	mflo $v1		# v1 = 3 * r²
+	jr $ra			# término da função
+		
+areaDoTri:
+	li $v0, 4		#imprime string
+	la $a0, perguntaA	
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	li $v0, 4
+	la $a0, perguntaB
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t1, $v0
+	
+	mult $t1, $t0		# a * b
+	mflo $t0		# t0 = a * b
+	addi $t1, $zero, 2
+	div $t0, $t1		# (a * b) / 2
+	mflo $v1		# v1 =  (a * b) / 2
+	jr $ra			# fim da função
+	
+arearet:
+	li $v0, 4		#imprime string
+	la $a0, perguntaA	
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	li $v0, 4
+	la $a0, perguntaB
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t1, $v0
+	
+	mult $t1, $t0		# a * b
+	mflo $v1		# v1 = a * b
+	jr $ra			# fim da função
